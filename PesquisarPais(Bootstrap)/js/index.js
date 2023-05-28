07e10cad6b91355a27f8b5e6857226bef1daece4
Luana <santos.luanabeatriz1995@gmail.com>
2023-05-28 13:39:37 +0100

    Adicionando a LP da CCXP

--- PesquisarPais(Bootstrap)/js/index.js
+++ PesquisarPais(Bootstrap)/js/index.js
@@ -4,17 +4,30 @@ function Procurar(){
     let pais = document.getElementById("pais").value
     let finalUrl = `https://restcountries.com/v3.1/name/${pais}?fullText=true`
 
-    fetch(finalUrl)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        document.getElementById("card").style.display = "block";
-        document.getElementById("bandeira").src = data[0].flags["svg"]
-        document.getElementById("nome").innerHTML = pais
-        document.getElementById("capital").innerHTML = data[0].capital[0]
-        document.getElementById("continente").innerHTML = data[0].continents[0]
-        document.getElementById("populacao").innerHTML = data[0].population
-    })
+    if(pais == ""){
+        alert("Preencha o país");
+    }else{
+        fetch(finalUrl)
+        .then(function(response){
+            if(response.status == 200){
+                return response.json()
+            }
+        })
+
+        .then(function(data){
+            if(!data){
+                alert("Digite um país válido!")
+                return false
+            }
+            document.getElementById("card").style.display = "block";
+            document.getElementById("bandeira").src = data[0].flags["svg"]
+            document.getElementById("nome").innerHTML = pais
+            document.getElementById("capital").innerHTML = data[0].capital[0]
+            document.getElementById("continente").innerHTML = data[0].continents[0]
+            document.getElementById("populacao").innerHTML = data[0].population
+        })
+    }
+
+    
     
 }
